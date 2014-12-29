7dc32337bfb7c3b1bf62193484d1dd56045dacd8
Melanie L. Fryman <melanie@frymanet.com>
2014-12-29 14:32:33 -0600

    fixed register e2e test assertion

--- test/e2e/authentication/register.spec.js
+++ test/e2e/authentication/register.spec.js
@@ -13,7 +13,7 @@ describe('register', function(){
 
   it('should register a new user', function(){
     element(by.model('user.username')).sendKeys('sam' + h.random(50000));
-    element(by.model('user.email')).sendKeys('sam@sam.com');
+    element(by.model('user.email')).sendKeys('sam@sam'+ h.random(50000) + '.com');
     element(by.model('user.password')).sendKeys('456789');
     element(by.css('button[ng-click]')).click();
     expect(element(by.css('.section-header > h1')).getText()).toEqual('LOGIN');
