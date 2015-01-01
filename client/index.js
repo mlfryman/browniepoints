1b53da20fce1a0ccc102a9901a01d54f43b93f86
Melanie L. Fryman <melanie@frymanet.com>
2014-12-31 18:40:31 -0600

    added profile, dashboard, messages, & prize states

--- client/index.js
+++ client/index.js
@@ -6,9 +6,13 @@
       $urlRouterProvider.otherwise('/');
 
       $stateProvider
-        .state('home',         {url:'/',         templateUrl:'/views/home/home.html'})
-        .state('register',     {url:'/register', templateUrl:'/views/users/users.html',   controller:'UsersCtrl'})
-        .state('login',        {url:'/login',    templateUrl:'/views/users/users.html',   controller:'UsersCtrl'});
+        .state('home',         {url:'/',            templateUrl:'/views/home/home.html'})
+        .state('register',     {url:'/register',    templateUrl:'/views/users/users.html',           controller:'UsersCtrl'})
+        .state('login',        {url:'/login',       templateUrl:'/views/users/users.html',           controller:'UsersCtrl'})
+        .state('profile',      {url:'/profile',     templateUrl:'/views/profile/profile.html',       controller:'ProfileCtrl'})
+        .state('dashboard',    {url:'/dashboard',   templateUrl:'/views/dashboard/dashboard.html',   controller:'DashboardCtrl'})
+        .state('prizes',       {url:'/prizes',      templateUrl:'/views/prizes/prizes.html',         controller:'PrizesCtrl'})
+        .state('messages',     {url:'/messages',    templateUrl:'/views/messages/inbox.html',        controller:'MessagesCtrl'});
       }])
     .run(['$rootScope', '$http', function($rootScope, $http){
       $http.get('/status').then(function(response){
